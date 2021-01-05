5ebc26371998426633b67cdd78b334f32659b0fc
Alex Rios <alexrios.dev@gmail.com>
2021-01-04 19:26:21 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
-export function manageFriends(state, action){
+// let state = {
+//   friends: [],
+// };
+
+// let action = {
+//   type: "ADD_FRIEND",
+//   friend: {
+//     id: 1,
+//     name: "Chrome Boi",
+//     hometown: "NYC",
+//   },
+// };
+
+// action = {
+//   type: "REMOVE_FRIEND",
+//   id: 1,
+// };
+
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+    case "REMOVE_FRIEND":
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.id),
+      };
+    default:
+      return state;
+  }
 }
